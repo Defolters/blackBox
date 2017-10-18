b55cbd8cde678480ec57029a7b605495a2797378
Daniel Emelyanov <defolterrik@gmail.com>
2017-10-18 10:45:29 +0300

    data.h

--- BlackBox/Data.h
+++ BlackBox/Data.h
@@ -1,14 +1,41 @@
-#pragma once
+#ifndef DATA_H
+#define DATA_H
 #include <utility>
+
+//!  A class that implements Data. 
+/*!
+This class has some getter and setter to control data.
+*/
 class Data
 {
 public:
+    //! Constructor
+    /*!
+    Create black box with Data
+    \param priority
+    \param id
+    */
     Data(int priority, int id);
+
+    //! Destructor
     ~Data();
+
+    //! addData
+    /*!
+    Set priority and id into Data
+    \param Data
+    \param id
+    */
 	void set(int priority, int id);
+
+    //! get
+    /*!
+    get pair of data
+    \return pair<int,int>
+    */
 	std::pair<int,int> get();
 private:
-    int priority;
-    int id;
+    int priority; /*!< priority of id. */
+    int id; /*!< id of user. */
 };
-
+#endif
