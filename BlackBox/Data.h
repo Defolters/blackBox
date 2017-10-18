35293096280cfd886d276ce9bb11bcaf048c2307
KovalevVasilii <32467326+KovalevVasilii@users.noreply.github.com>
2017-10-18 10:35:53 +0300

    Add files via upload

--- BlackBox/Data.h
+++ BlackBox/Data.h
@@ -1,13 +1,14 @@
 #pragma once
+#include <utility>
 class Data
 {
 public:
-    Data();
+    Data(int priority, int id);
     ~Data();
-
+	void set(int priority, int id);
+	std::pair<int,int> get();
 private:
     int priority;
     int id;
-
 };
 
