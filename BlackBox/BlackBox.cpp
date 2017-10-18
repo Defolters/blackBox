b8aca2c8305d242b2bd41de53bfd7c86d78a2270
Daniel Emelyanov <defolterrik@gmail.com>
2017-10-18 10:17:49 +0300

    black

--- BlackBox/BlackBox.cpp
+++ BlackBox/BlackBox.cpp
@@ -1,12 +1,33 @@
-#include "BlackBox.h"
+#include "blackbox.h"
 
+blackBox::blackBox()
+{}
 
-
-BlackBox::BlackBox()
+blackBox::~blackBox()
 {
 }
 
-
-BlackBox::~BlackBox()
+blackBox::blackBox(Data data_)
 {
+    conts.push_back(data_);
+}
+
+Data blackBox::Get()
+{
+    if (conts.size() == 0)
+        return ;
+    else
+        return (conts[conts.size()-1]);
+}
+
+void blackBox::Add(Data data_)
+{
+    auto pos = conts.begin();
+    while (1)
+    {
+        if (data_.priority > pos->priority)
+            break;
+        pos++;
+    }
+    conts.insert(pos,data_);
 }
--- BlackBox/BlackBox.h
+++ BlackBox/BlackBox.h
@@ -1,12 +1,24 @@
-#pragma once
-class BlackBox
+#ifndef BLACKBOX_H
+#define BLACKBOX_H
+#include "Data.h"
+#include <list>
+
+//!  A class that implements black box. 
+/*!
+This class has some getter and setter to control box.
+*/
+class blackBox
 {
+private:
+    //! Constructor
+    blackBox();
+    //! Destructor
+    ~blackBox();
+    blackBox(Data);
+    std::list<Data> conts;
 public:
-    BlackBox();
-    ~BlackBox();
     void Add(Data);
     Data Get();
-private:
-    std::vector<Data> dataOfBlackBox
 };
 
+#endif // BLACKBOX_H
